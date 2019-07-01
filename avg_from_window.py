95f3482d3ee749ff2e3a4abeeea7a0a337962689
Ashkara Vingh II <michael.j.lukiman@gmail.com>
2019-07-01 17:53:46 -0400

    Average working perfectly, ignores NaN

--- avg_from_window.py
+++ avg_from_window.py
@@ -6,46 +6,19 @@ import csv, pandas as pd, sys
 import datetime as dt
 from typing import List
 
-def filterDimensions(csvpath:str, newcsv:str, features:List):
-    '''
-    Take a set of features you want to keep and isolate those.
-    '''
-    chunksize = 20000
-    i=0
-    for chunk in pd.read_csv(csvpath, chunksize=chunksize, usecols=features):
-        print("Chunk",i)
-        i+=1
-        chunk.to_csv(newcsv, mode='a', index=False)
-
-def filterMatch(csvpath:str, newcsv:str, feature:str, match:str):
-    '''
-    Only include results which match a certain value.
-    '''
-    chunksize = 20000
-    i=0
-    writeHeader = True
-    for chunk in pd.read_csv(csvpath, chunksize=chunksize):
-        print("Chunk",i)
-        chunk = chunk.loc[chunk[feature] == match]
-        i+=1
-        chunk.to_csv(newcsv, mode='a', index=False, header=writeHeader)
-        writeHeader = False
-
 def filterDate(csvpath:str, newcsv:str, name_of_date_col:str, start_date:str, end_date:str):
     '''
     Assuming a 'date' column, only filter rows within a set of dates.
     '''
-    print(start_date, end_date)
     chunksize = 20000
     i=0
     writeHeader = True
     for chunk in pd.read_csv(csvpath, chunksize=chunksize):
-        print("Chunk",i)
+        print("Chunk",i,'\n---')
         chunk[name_of_date_col] = pd.to_datetime(chunk[name_of_date_col]) # Convert the date string column to datetime objects. Will make it easier to select range. 
         mask = (chunk[name_of_date_col] >= start_date) & (chunk[name_of_date_col] <= end_date) # Make a new mask that will only include certain dates.
-        print(mask)
         masked_chunk = chunk.loc[mask]
-        masked_chunk.to_csv(newcsv, mode='a', index=False, header=writeHeader)
+        masked_chunk.to_csv(newcsv, mode='a+', index=False, header=writeHeader)
         i+=1
         writeHeader = False
 
@@ -54,18 +27,13 @@ def findGeometricAvg(csvpath: str, columnName:str):
     total = 0
     count = 0
     i=0
-    writeHeader = True
     for chunk in pd.read_csv(csvpath, chunksize=chunksize):
-        print("Chunk",i)
+        print("Chunk",i,'\n---')
         i+=1
-        writeHeader = False
-        for row in chunk:
-            count += 1
-            try:
-                total += float(row[columnName])
-            except:
-                print(row)
-                print("Cannot column attribute convert to float. Make sure it's a number.")
+        casted_chunk = pd.to_numeric(chunk[columnName], downcast='float',errors='ignore')
+        print(casted_chunk)
+        total += casted_chunk.sum()
+        count += len(chunk)
     return total / count
 
 if __name__ == "__main__":
@@ -84,13 +52,15 @@ if __name__ == "__main__":
     # Creates file only with desired dates. Will make new file so don't have to keep in RAM.
     # end_date = dt.date.today() # Now, today.
     # end_str = str(end_date)
+    now = dt.datetime.now().strftime("%Y%m%d%H%M%S")
     # start_str = str(end_date - dt.timedelta(days=days_before)) # N days before today.
     end_str = '2018-11-26'
     start_str = '2018-11-20'
-    date_pared = sys.argv[1] + f"_{start_str}_{end_str}"
+    date_pared = f"{start_str}_TO_{end_str}_CREATED-{now}_" + sys.argv[1].split('/')[-1]
     filterDate(data,date_pared,'Date',start_str,end_str) 
 
     for feature in quantified_features: 
         avgs[feature] = findGeometricAvg(date_pared, feature)
 
-    print(avgs)
+    print('-------------------')
+    [print(f'AVG for {key}: {val}') for key, val in avgs.items()]
